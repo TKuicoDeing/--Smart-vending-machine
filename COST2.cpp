4a97a57f489c05678f6d3001f1c81c26612905ff
tdennis618 <127190935+tdennis618@users.noreply.github.com>
2023-05-16 10:39:26 +0800

    Create COST2.cpp

---
+++ COST2.cpp
@@ -0,0 +1,31 @@
+#include "Cost.h"
+#include <iostream>
+#include <cstdlib> // 用於清除屏幕的system("cls")
+using namespace std;
+
+Cost::Cost() {
+    money = 0;
+}
+
+void Cost::addMoney(int inputMoney) {
+    money += inputMoney;
+    system("cls");
+    cout << "Money: " << money << endl;
+}
+
+void Cost::getSpread() {
+    int a = money; // 保存進入時的狀態
+    int newMoney = 0;
+
+    // 執行找零操作，這裡簡單示意為將金額清零
+    newMoney = money;
+    money = 0;
+
+    system("cls");
+    cout << "Previous amount: " << a << endl;
+    cout << "Change given: " << newMoney << endl;
+}
+
+int Cost::returnAmount() {
+    return money;
+}
